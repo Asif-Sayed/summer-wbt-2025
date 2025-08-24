9d185d772f97bbf1942501bd86135676e9af6311
Asif-Sayed <asifsayed286@gmail.com>
2025-08-25 00:36:04 +0600

    PHP Task 6

--- php/6.php
+++ php/6.php
@@ -1,3 +1,46 @@
 <?php
-//5. Write a PHP script to print all the odd numbers between 10 to 100 Hints: use LOOP & IF-ELSE
-function
+//6. Write a PHP script to search an element from an array Hints: use LOOP, IF-ELSE & ARRAY
+
+// $elements = array("apple", "banana", "orange", "grape", "mango");
+// $searchElement = "banana";   
+// function searchElementInArray($elements, $searchElement) {
+//     foreach ($elements as $element) {
+//         if ($element === $searchElement) {
+//             return "$searchElement found in the array.";
+//         }
+//     }
+//     return "$searchElement not found in the array.";
+// }
+// echo searchElementInArray($elements, $searchElement);
+
+
+// function searchElementInArray($elements, $searchElement) {
+//     $found = false;
+//     foreach ($elements as $element) {
+//         if ($element === $searchElement) {
+//             $found = true;
+//             break;
+//         }
+//     }
+//     if ($found) {
+//         return "$searchElement found in the array.";
+//     } else {
+//         return "$searchElement not found in the array.";
+//     }
+// }
+// $elements = array("apple", "banana", "orange", "grape", "mango");
+// $searchElement = "baana";
+// echo searchElementInArray($elements, $searchElement);
+
+$numbers = [10, 20, 30, 40, 50];
+function searchElement($array, $element) {
+    if (in_array($element, $array)) {
+        echo "Element $element found in the array.<br>";
+    } else {
+        echo "Element $element not found in the array.<br>";
+    }
+}
+searchElement($numbers, 30);
+searchElement($numbers, 60);
+
+?>
